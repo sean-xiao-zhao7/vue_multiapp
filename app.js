444846a2580c64b59dabf1ebc7fd183f87da1ba3
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-02-18 17:18:33 -0500

    Init

---
+++ app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            currentUserInput: "",
+            message: "Vue is great!",
+        };
+    },
+    methods: {
+        saveInput(event) {
+            this.currentUserInput = event.target.value;
+        },
+        setText() {
+            this.message = this.$refs.userText.value;
+        },
+    },
+});
+
+app.mount("#app");
+const app2 = Vue.createApp({
+    template: `
+    <p>{{ favoriteMeal }}</p>
+    `,
+    data() {
+        return {
+            favoriteMeal: "Pizza",
+        };
+    },
+});
+app2.mount("#app2");
